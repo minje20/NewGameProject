64e91fdf84227c26c14d062d188e825c71d068c3
ahhjm881 <game.rcandyr@gmail.com>
2024-05-19 21:53:08 +0900

    :sparkles: 얼음 계량 미니게임 개선

--- Assets/Dev/Feature/MiniGame/IceDispencer/IceDispenserController.cs
+++ Assets/Dev/Feature/MiniGame/IceDispencer/IceDispenserController.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using JetBrains.Annotations;
+using MyBox;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.Timeline;
 using Random = UnityEngine.Random;
 
@@ -14,10 +17,28 @@ public class IceDispenserController : MonoBehaviour
     [SerializeField] private List<Vector3> _randomSizeVariation;
     [SerializeField] private float _creationDelay;
     [SerializeField] private float _gameDuration;
+    [SerializeField] private Vector2 _createStepWaitDurationRange;
+    [SerializeField] private Vector2Int _createStepIceCountRange;
+    [SerializeField] private int _createStepCount;
+    [SerializeField] private int _creationFov;
+    [SerializeField] private int _creationForcePower;
+    [SerializeField] private float _creationDistanceFromPosition;
     [SerializeField] private GameObject _icePrefab;
     [SerializeField] private PressedButton _button;
     [SerializeField] private MiniGameTimer _gameTimer;
     [SerializeField] private IceDispenserPosition _position;
+
+    [ButtonMethod]
+    private void RemoveAll()
+    {
+        if (Application.isPlaying == false) return;
+
+        while (_createdRandomIce.Any())
+        {
+            var obj = _createdRandomIce.Dequeue();
+            Destroy(obj);
+        }
+    }
 
     private Queue<GameObject> _createdRandomIce = new();
 
@@ -48,6 +69,7 @@ public class IceDispenserController : MonoBehaviour
         
         gameObject.SetActive(false);
     }
+    
 
     private GameObject CreateIce()
     {
@@ -68,6 +90,14 @@ public class IceDispenserController : MonoBehaviour
 
         return _randomSizeVariation[randomIndex];
     }
+    private float GetRandomCreationStepWaitDuration()
+    {
+        return Random.Range(_createStepWaitDurationRange.x, _createStepWaitDurationRange.y);
+    }
+    private int GetRandomCreationStepIceCount()
+    {
+        return Random.Range(_createStepIceCountRange.x, _createStepIceCountRange.y);
+    }
 
     private void Awake()
     {
@@ -85,13 +115,36 @@ public class IceDispenserController : MonoBehaviour
             
             if (_button.IsPressed)
             {
-                _ = CreateIce();
-                yield return new WaitForSeconds(_creationDelay);
+                int length = GetRandomCreationStepIceCount();
+                for (int i = 0; i < length; i++)
+                {
+                    var obj = CreateIce();
+                    var angle = Mathf.Lerp(_creationFov * -0.5f, _creationFov * 0.5f, Random.value);
+                    obj.transform.position = _position.IceSpawnPoint + Quaternion.Euler(0f, 0f, angle) * Vector3.down * _creationDistanceFromPosition;
+                    obj.transform.rotation = quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+                    
+                    obj.GetComponent<Rigidbody2D>()?.AddForce(
+                        _creationForcePower *
+                        (Quaternion.Euler(0f, 0f, angle) * Vector3.down)
+                        , ForceMode2D.Impulse);
+                    
+                    yield return new WaitForSeconds(_creationDelay);
+                }
+                yield return new WaitForSeconds(GetRandomCreationStepWaitDuration());
             }
 
             yield return new WaitForEndOfFrame();
         }
     }
-    
-    
+
+    private void OnDrawGizmosSelected()
+    {
+        var pos = _position.IceSpawnPoint;
+        Gizmos.DrawRay(pos, Vector3.down * _creationDistanceFromPosition);
+        var el = Quaternion.Euler(0f, 0f, _creationFov * -0.5f);
+        var er = Quaternion.Euler(0f, 0f, _creationFov * 0.5f);
+        
+        Gizmos.DrawRay(pos, er * Vector3.down * 5f);
+        Gizmos.DrawRay(pos, el * Vector3.down * 5f);
+    }
 }
