b289d720179034bfbcb435c55ea84c2200b501d3
ahhjm881 <game.rcandyr@gmail.com>
2024-05-20 20:04:16 +0900

    :sparkles: 대사 기능 수정
    변경사항:
    - 대사 스킵 UI 추가(주기적으로 깜빡이도록)
    - 대사의 화자에 따른 말풍선 화살표 방향 다르게 하는 기능 추가

--- Assets/Dev/Feature/Dialogue/DialogueController.cs
+++ Assets/Dev/Feature/Dialogue/DialogueController.cs
@@ -8,20 +8,36 @@ using MyBox;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class DialogueItem
+{
+    public string Text { get; private set; }
+    public bool IsMaster { get; private set; }
+
+    public DialogueItem(string text, bool isMaster)
+    {
+        Text = text;
+        IsMaster = isMaster;
+    }
+}
 
 [Serializable]
 public class DialogueContext
 {
-    private List<string> _texts;
+    private List<DialogueItem> _texts;
     private float _duration;
     private TMP_Text _text;
+    private Image _upDir;
+    private Image _downDir;
 
     private int _index;
 
     private CancellationTokenSource _source;
 
     public bool CanNext { get; private set; }
-    
+
     public string CurrentText { get; private set; }
 
     public UniTask Next()
@@ -38,7 +54,12 @@ public class DialogueContext
             _source = new CancellationTokenSource();
         }
 
-        CurrentText = _texts[_index++];
+        var item = _texts[_index++];
+        CurrentText = item.Text;
+
+        _upDir.gameObject.SetActive(!item.IsMaster);
+        _downDir.gameObject.SetActive(item.IsMaster);
+        
         var task = _text.DoTextUniTask(CurrentText, _duration, _source.Token);
 
 
@@ -56,12 +77,53 @@ public class DialogueContext
         _source = null;
     }
 
-    public DialogueContext(List<string> texts, float duration, TMP_Text text)
+    public DialogueContext(List<DialogueItem> texts, float duration, TMP_Text text, Image upDir, Image downDir)
     {
         _texts = texts;
         _duration = duration;
         _text = text;
         CanNext = texts.Count > 0;
+        _upDir = upDir;
+        _downDir = downDir;
+    }
+}
+
+public class DialogueItemMaker : Unit
+{
+    [DoNotSerialize] [PortLabelHidden] public ControlInput _cInput { get; private set; }
+    [DoNotSerialize] [PortLabelHidden] public ControlOutput _cOutput { get; private set; }
+    
+    [DoNotSerialize] [PortLabelHidden] public ValueInput TextList { get; private set; }
+    [DoNotSerialize] [PortLabelHidden] public ValueInput MasterList { get; private set; }
+    [DoNotSerialize] [PortLabelHidden] public ValueOutput Result { get; private set; }
+
+    private List<DialogueItem> _result;
+    
+    protected override void Definition()
+    {
+        _cInput = ControlInput("", Make);
+        _cOutput = ControlOutput("");
+
+        TextList = ValueInput<List<string>>("text");
+        MasterList = ValueInput<List<bool>>("is master");
+        Result = ValueOutput<List<DialogueItem>>("items", (x)=>_result);
+    }
+
+    private ControlOutput Make(Flow flow)
+    {
+        var texts = flow.GetValue<List<string>>(TextList);
+        var masters = flow.GetValue<List<bool>>(MasterList);
+
+        var result = new List<DialogueItem>(texts.Count);
+
+        for (int i = 0; i < Mathf.Min(texts.Count, masters.Count); i++)
+        {
+            result.Add(new DialogueItem(texts[i], masters[i]));
+        }
+
+        _result = result;
+
+        return _cOutput;
     }
 }
 
@@ -99,7 +161,7 @@ public class DialogueContextUnit : WaitUnit
 
             yield return new WaitForEndOfFrame();
             yield return new WaitForEndOfFrame();
-            
+
             var task = context.Next();
 
             if (task.Status == UniTaskStatus.Faulted)
@@ -127,6 +189,23 @@ public class DialogueController : MonoBehaviour
     private TMP_Text _text;
 
 
+    [field: SerializeField, InitializationField, MustBeAssigned]
+    private Image _upDirection;
+
+    [field: SerializeField, InitializationField, MustBeAssigned]
+    private Image _downDirection;
+    
+
+    [field: SerializeField, InitializationField, MustBeAssigned]
+    private Image _skipTip;
+
+    [SerializeField] private float _skipTwinkleDuration = 0.5f;
+
+    private void Awake()
+    {
+        StartCoroutine(CoUpdate());
+    }
+
     public void Show()
     {
         _content.SetActive(true);
@@ -139,11 +218,25 @@ public class DialogueController : MonoBehaviour
         _text.text = "";
     }
 
-    public DialogueContext BeginText(List<string> textList, float duration) =>
+    public DialogueContext BeginText(List<DialogueItem> textList, float duration) =>
         new DialogueContext(
             textList,
             duration,
-            _text);
+            _text,
+            _upDirection,
+            _downDirection
+        );
+
+    private IEnumerator CoUpdate()
+    {
+        while (true)
+        {
+            _skipTip.gameObject.SetActive(true);
+            yield return new WaitForSeconds(_skipTwinkleDuration);
+            _skipTip.gameObject.SetActive(false);
+            yield return new WaitForSeconds(_skipTwinkleDuration);
+        }
+    } 
 }
 
 
