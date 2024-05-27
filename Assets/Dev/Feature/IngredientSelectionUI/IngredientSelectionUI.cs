6dcadb810e835bb562cac263bc356f6cd46cde8d
juho Kim <42205951+gabjuho@users.noreply.github.com>
2024-05-28 00:39:02 +0900

    :sparkles: 재료 인벤토리 UI 예외처리
    변경사항:
    1. 서로 다른 재료를 UI 내 배치
    2. 좌우 이동 선택 시 이동할 수 있는 범위 제한

--- Assets/Dev/Feature/IngredientSelectionUI/IngredientSelectionUI.cs
+++ Assets/Dev/Feature/IngredientSelectionUI/IngredientSelectionUI.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,7 +12,10 @@ public class IngredientSelectionUI : MonoBehaviour
     [SerializeField] private Color32[] _ingredientColors;
     [SerializeField] private GameObject _ingredientPrefab;
     [SerializeField] private Transform _ingredientGroup;
+    [SerializeField] private Sprite[] _ingredientSprites;
      private GameObject[] _ingredientPrefabs;
+
+     private Vector2 _ingredientCaptureIndexRange = new Vector2(0, 4);
 
     private void Start()
     {
@@ -26,13 +30,26 @@ public class IngredientSelectionUI : MonoBehaviour
             
             _ingredientPrefabs[i].transform.localScale = _ingredientTransforms[i].localScale;
             _ingredientPrefabs[i].GetComponent<Image>().color = _ingredientColors[i];
+            _ingredientPrefabs[i].GetComponent<Image>().sprite = _ingredientSprites[i];
         }
     }
 
-    public void TurnLeft()
+    public void TurnRight()
     {
+        if ((int)_ingredientCaptureIndexRange.x == _ingredientPrefabs.Length - 3)
+        {
+            return;
+        }
+        _ingredientCaptureIndexRange.x++;
+        _ingredientCaptureIndexRange.y++;
+        
         for (int i = 0; i < _ingredientPrefabs.Length; i++)
         {
+            if (_ingredientPrefabs[i] == null)
+            {
+                continue;
+            }
+            
             if (i == 0)
             {
                 Destroy(_ingredientPrefabs[i]);
@@ -44,23 +61,36 @@ public class IngredientSelectionUI : MonoBehaviour
             _ingredientPrefabs[i].GetComponent<Image>().DOColor(_ingredientColors[i - 1], 0.5f);
             
             _ingredientPrefabs[i - 1] = _ingredientPrefabs[i];
-
-            if (i == _ingredientPrefabs.Length - 1)
+            _ingredientPrefabs[i] = null;
+            
+            if (i == _ingredientPrefabs.Length - 1 && _ingredientCaptureIndexRange.y - i <= 0)
             {
                 _ingredientPrefabs[i] = Instantiate(_ingredientPrefab,
                     _ingredientTransforms[i].position,
                     _ingredientTransforms[i].rotation,
                     _ingredientGroup);
-                
+
+                _ingredientPrefabs[i].GetComponent<Image>().sprite = _ingredientSprites[i + (int)_ingredientCaptureIndexRange.x];
                 _ingredientPrefabs[i].transform.localScale = _ingredientTransforms[i].localScale;
             }
         }
     }
     
-    public void TurnRight()
+    public void TurnLeft()
     {
+        if ((int)_ingredientCaptureIndexRange.x == -2)
+        {
+            return;
+        }
+        _ingredientCaptureIndexRange.x--;
+        _ingredientCaptureIndexRange.y--;
         for (int i = _ingredientPrefabs.Length - 1; i >= 0; i--)
         {
+            if (_ingredientPrefabs[i] == null)
+            {
+                continue;
+            }
+            
             if (i == _ingredientPrefabs.Length - 1)
             {
                 Destroy(_ingredientPrefabs[i]);
@@ -70,16 +100,18 @@ public class IngredientSelectionUI : MonoBehaviour
             _ingredientPrefabs[i].transform.DOMove(_ingredientTransforms[i + 1].position, 0.5f);
             _ingredientPrefabs[i].transform.DOScale(_ingredientTransforms[i + 1].localScale, 0.5f);
             _ingredientPrefabs[i].GetComponent<Image>().DOColor(_ingredientColors[i + 1], 0.5f);
-
+            
             _ingredientPrefabs[i + 1] = _ingredientPrefabs[i];
 
-            if (i == 0)
+            _ingredientPrefabs[i] = null;
+            if (i == 0 && _ingredientCaptureIndexRange.x + i >= 0)
             {
                 _ingredientPrefabs[i] = Instantiate(_ingredientPrefab,
                     _ingredientTransforms[i].position,
                     _ingredientTransforms[i].rotation,
                     _ingredientGroup);
                 
+                _ingredientPrefabs[i].GetComponent<Image>().sprite = _ingredientSprites[i + (int)_ingredientCaptureIndexRange.x];
                 _ingredientPrefabs[i].transform.localScale = _ingredientTransforms[i].localScale;
             }
         }
